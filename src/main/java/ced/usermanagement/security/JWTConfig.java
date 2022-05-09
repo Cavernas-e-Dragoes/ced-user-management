8c27c1265420a226318be619281a17cce9dc8839
Bruno Jaques Olivera de Vargas <70863493+bjvargas@users.noreply.github.com>
2022-05-08 21:06:03 -0300

    Updating Cors Config

--- src/main/java/ced/usermanagement/security/JWTConfig.java
+++ src/main/java/ced/usermanagement/security/JWTConfig.java
@@ -14,6 +14,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @EnableWebSecurity
 public class JWTConfig extends WebSecurityConfigurerAdapter {
 
@@ -51,6 +53,8 @@ public class JWTConfig extends WebSecurityConfigurerAdapter {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
         CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST"));
         source.registerCorsConfiguration("/**", corsConfiguration);
         return source;
     }
